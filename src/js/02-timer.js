9eb0224b5a3440d5405a07e36f02dcccf7f6376d
bogunenko-daryna <124713486+hladykzarina@users.noreply.github.com>
2023-08-20 17:25:17 +0200

    timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -38,11 +38,29 @@ const fp = flatpickr(input, {
           timerSeconds.textContent = addLeadingZero(convertMs(ms).seconds);
           if (ms < 1000) {
             clearInterval(timerId);
+            timerDays.textContent = '00';
             timerHours.textContent = '00';
             timerMinutes.textContent = '00';
+            timerSeconds.textContent = '00';
           }
         }, 1000);
       });
     }
   },
 });
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000));
+console.log(convertMs(140000));
+console.log(convertMs(24140000));
